82c3a1ee2852b86e71cd1835d4fa7d40af687174
Carlos Torres <carlos_torres1@jabil.com>
2019-12-12 14:58:53 -0600

    Passing Dynamic data with props

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { format } from 'path';
-import { unlink, link } from 'fs';
 
 class StorePicker extends React.Component {
-    render() { 
-        return (
-            // To be able to return more than one element. React.Fragment renders to nothing
-            <React.Fragment>
-                <p>Fish!</p>
-                <form action="" className="store-selector">
-                    { /*Hey look JSX! I'm using brackets, that means I want to do some Javascript */}
-                    <h2>Please Enter a Store</h2>
-                </form>
-            </React.Fragment>
-        )
-    }
+  render() { 
+    return (
+      // To be able to return more than one element. React.Fragment renders to nothing
+      <React.Fragment>
+        <p>Fish!</p>
+        <form action="" className="store-selector">
+            { /*Hey look JSX! I'm using brackets, that means I want to do some Javascript */}
+            <h2>Please Enter a Store</h2>
+            <input type="text" required placeholder="Store Name" />
+            <button type="submit">Visit Store -></button>
+        </form>
+      </React.Fragment>
+    )
+  }
 }
 
 export default StorePicker;
