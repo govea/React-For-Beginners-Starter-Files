eac9826f5dc4f50a394e3fcd7c23f34926cbe0df
Carlos Torres <carlos_torres1@jabil.com>
2019-12-12 22:47:52 -0600

    Understanding State

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,27 @@ import Order from './Order'
 import Inventory from './Inventory'
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    console.log('Adding a fish!');
+    // 1. Take a copy of the existing state
+    const fishes = {... this.state.fishes}; // Object spread to do a copy of an object.
+    
+    // 2. Add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+
+    // 3. Set the new fishes object to state
+    this.setState({
+      fishes        // fishes: fishes
+    });
+
+    // this.state.fishes.fish1 = fish;   // you don't a mutation like this.
+  }
+
   render(){
     return (
       <div className="catch-of-the-day">
@@ -11,7 +32,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order /> 
-        <Inventory />
+        <Inventory addFish={this.addFish}/>
       </div>
     )
   }
